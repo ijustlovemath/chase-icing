df80c519e6836faf8455f449fd0ea5278b32440d
Jeremy DeJournett <dejourn2@illinois.edu>
2019-05-27 01:20:03 -0400

    removing misleading _t suffix

--- test/test_icing.cpp
+++ test/test_icing.cpp
@@ -13,15 +13,15 @@ typedef std::vector<icing_float> state_type;
 // [2] - Reference Paper: computer methods and programs in biomedicine 102 (2011) 192–205
 template<typename U = icing_float>
 class ChaseIcing {
-    // Each enum maps to an index within the state variable for accessing that
+    // Each enum maps to an index within the state vector for accessing that
     // function's value.
-    enum Function { G_t = 0
-        , Q_t
-        , I_t
-        , P1_t
-        , P2_t
-        , P_t
-        , u_en_t
+    enum Function { fn_G = 0
+        , fn_Q
+        , fn_I
+        , fn_P1
+        , fn_P2
+        , fn_P
+        , fn_u_en
     };
 
     /* a bunch of constants that should be in the namespace */
@@ -32,7 +32,7 @@ class ChaseIcing {
 
     auto P_min(const auto &x, auto t)
     {
-        return std::min(d2 * x[P2_t], P_max);
+        return std::min(d2 * x[fn_P2], P_max);
     }
 
     auto alpha_decay(const auto variable, const auto decay_parameter)
@@ -43,16 +43,16 @@ class ChaseIcing {
     auto Q_frac(const auto &x, const auto t)
     {
         const U a_G = 1.0 / 65.0; // Table II, [1]
-        const U Q = x[Q_t];
+        const U Q = x[fn_Q];
         return alpha_decay(Q, a_G);
     }
 
     // PN(t) -> Parenteral nutrition input, eg IV dextrose
     auto _P(const auto &x, const auto t)
     {
-        const U _PN_t = 0.0; // TODO -> derive this over the network
+        const U _PN_ext = 0.0; // TODO -> derive this over the network
 
-        return P_min(x, t) + _PN_t;
+        return P_min(x, t) + _PN_ext;
     }
 
     auto _u_en(const auto &x, const auto t)
@@ -61,7 +61,7 @@ class ChaseIcing {
         const U k2 = -49.9; // mU/min, Table II, [1]
         const U u_min = 16.7; // mU/min, Table II, [1]
         const U u_max = 266.7; // mU/min, Table II, [1]
-        const U G = x[G_t];
+        const U G = x[fn_G];
         return std::min(std::max(u_min, k1 * G + k2), u_max);
     }
 
@@ -69,8 +69,8 @@ class ChaseIcing {
     {
         const U p_G = 0.006; // End of section 4.1, in [2]
         const U S_I = 0.5e-3; // TODO: patient specific
-        const U G = x[G_t];
-        const U Q = x[Q_t];
+        const U G = x[fn_G];
+        const U Q = x[fn_Q];
         const U P = _P(x, t);
         const U EGP = 1.16; // mmol/min Table II, [1]
         const U CNS = 0.3; // mmol/min Table II, [1]
@@ -89,8 +89,8 @@ class ChaseIcing {
 
     auto Q_dot(const auto &x, const auto t)
     {
-        const U I = x[I_t];
-        const U Q = x[Q_t];
+        const U I = x[fn_I];
+        const U Q = x[fn_Q];
         const U n_C = 0.0075; // 1/min, Table II, [1]
 
         return n_I * (I - Q) - n_C * Q_frac(x, t);
@@ -106,8 +106,8 @@ class ChaseIcing {
 
         const U u_ex = 0.0; // TODO: get this over the network
 
-        const U Q = x[Q_t];
-        const U I = x[I_t];
+        const U Q = x[fn_Q];
+        const U I = x[fn_I];
         const U u_en = _u_en(x, t);
 
         auto dIdt = U(0.0);
@@ -127,28 +127,28 @@ class ChaseIcing {
     auto P1_dot(const auto &x, auto t)
     {
         const auto D = U(0.0); // enteral feed rate TODO: get from network
-        return -d1 * x[P1_t] + D;
+        return -d1 * x[fn_P1] + D;
     }
 
     auto P2_dot(const auto &x, auto t)
     {
-        return -P_min(x, t) + d1 * x[P1_t];
+        return -P_min(x, t) + d1 * x[fn_P1];
     }
 
 public:
     using state_type = std::vector<U>;
     void operator() (const state_type &x, state_type &dxdt, const U t)
     {
-        dxdt[G_t] = G_dot(x, t);
-        dxdt[Q_t] = Q_dot(x, t);
-        dxdt[I_t] = I_dot(x, t);
-        dxdt[P1_t] = P1_dot(x, t);
-        dxdt[P2_t] = P2_dot(x, t);
+        dxdt[fn_G] = G_dot(x, t);
+        dxdt[fn_Q] = Q_dot(x, t);
+        dxdt[fn_I] = I_dot(x, t);
+        dxdt[fn_P1] = P1_dot(x, t);
+        dxdt[fn_P2] = P2_dot(x, t);
 
         if(std::fmod(t, 10.0) <= 0.1) {
             std::cout << 
             "t: " << t << "\t"
-            "G: " << x[G_t] << std::endl;
+            "G: " << x[fn_G] << std::endl;
         }
     }
 };
@@ -162,9 +162,10 @@ int main(void)
     Chase model;  
     
     const double dt = 0.1;
+    const double tend = 1e5;
     vec5 x = {10.0, 3.0, 10.0, 4.0, 3.0};
 
-    integrate_const(stepper, model, x, 0.0, 1000.0, dt);
+    integrate_const(stepper, model, x, 0.0, tend, dt);
 	
     return 0;
 }
