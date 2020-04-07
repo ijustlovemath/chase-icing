df428d505b3213b74b4a4bc4c9ef455d1bf0203a
Jeremy DeJournett <dejourn2@illinois.edu>
2020-04-06 22:11:52 -0400

    incrememntal update

--- test/test_icing.cpp
+++ test/test_icing.cpp
@@ -2,12 +2,13 @@
 #include <algorithm> /* for numeric min/max */
 #include <cmath> /* exp, fmod */
 #include <iostream> /* cout */
+#include <string>
 
 #include <boost/numeric/odeint.hpp>
 using namespace boost::numeric::odeint;
 
 typedef double icing_float;
-typedef std::vector<icing_float> state_type;
+//typedef std::vector<icing_float> state_type;
 
 // [1] - Reference Paper: TBME-01160-2016 
 // [2] - Reference Paper: computer methods and programs in biomedicine 102 (2011) 192–205
@@ -30,17 +31,17 @@ class ChaseIcing {
     const U d2 = -std::log(0.5)/100.0; // 1/min, Table II, [1]
     const U P_max = 6.11; // mmol/min, Table II, [1]
 
-    auto P_min(const auto &x, auto t)
+    auto P_min(const std::vector<U> &x)
     {
         return std::min(d2 * x[fn_P2], P_max);
     }
 
-    auto alpha_decay(const auto variable, const auto decay_parameter)
+    auto alpha_decay(const U variable, const U decay_parameter)
     {
         return variable / (1.0 + decay_parameter * variable);
     }
 
-    auto Q_frac(const auto &x, const auto t)
+    auto Q_frac(const std::vector<U> &x)
     {
         const U a_G = 1.0 / 65.0; // Table II, [1]
         const U Q = x[fn_Q];
@@ -48,14 +49,14 @@ class ChaseIcing {
     }
 
     // PN(t) -> Parenteral nutrition input, eg IV dextrose
-    auto _P(const auto &x, const auto t)
+    auto _P(const std::vector<U> &x)
     {
         const U _PN_ext = 0.0; // TODO -> derive this over the network
 
-        return P_min(x, t) + _PN_ext;
+        return P_min(x) + _PN_ext;
     }
 
-    auto _u_en(const auto &x, const auto t)
+    auto _u_en(const std::vector<U> &x)
     {
         const U k1 = 14.9; // mU * l / mmol/min, Table II, [1]
         const U k2 = -49.9; // mU/min, Table II, [1]
@@ -65,13 +66,13 @@ class ChaseIcing {
         return std::min(std::max(u_min, k1 * G + k2), u_max);
     }
 
-    auto G_dot(const auto &x, const auto t)
+    auto G_dot(const std::vector<U> &x)
     {
         const U p_G = 0.006; // End of section 4.1, in [2]
         const U S_I = 0.5e-3; // TODO: patient specific
         const U G = x[fn_G];
         const U Q = x[fn_Q];
-        const U P = _P(x, t);
+        const U P = _P(x);
         const U EGP = 1.16; // mmol/min Table II, [1]
         const U CNS = 0.3; // mmol/min Table II, [1]
         const U V_G = 13.3; // L, Table II, [1]
@@ -81,22 +82,22 @@ class ChaseIcing {
         //	 - S_I G(t) (Q(t) / (1 + a_G Q(t)))
         //	 + (P(t) + EGP -CNS)/V_G
         dGdt += -p_G * G;
-        dGdt += -S_I*G * Q_frac(x, t);
+        dGdt += -S_I*G * Q_frac(x);
         dGdt += (P + EGP - CNS)/V_G;
 
         return dGdt;
     }
 
-    auto Q_dot(const auto &x, const auto t)
+    auto Q_dot(const std::vector<U> &x)
     {
         const U I = x[fn_I];
         const U Q = x[fn_Q];
         const U n_C = 0.0075; // 1/min, Table II, [1]
 
-        return n_I * (I - Q) - n_C * Q_frac(x, t);
+        return n_I * (I - Q) - n_C * Q_frac(x);
     }
 
-    auto I_dot(const auto &x, auto t)
+    auto I_dot(const std::vector<U> &x)
     {
         const U n_K = 0.0542; // 1/min, Table II, [1]
         const U n_L = 0.1578; // 1/min, Table II, [1] 
@@ -108,7 +109,7 @@ class ChaseIcing {
 
         const U Q = x[fn_Q];
         const U I = x[fn_I];
-        const U u_en = _u_en(x, t);
+        const U u_en = _u_en(x);
 
         auto dIdt = U(0.0);
         // I' = - n_K I(t)
@@ -124,26 +125,26 @@ class ChaseIcing {
         return dIdt;
     }
 
-    auto P1_dot(const auto &x, auto t)
+    auto P1_dot(const std::vector<U> &x)
     {
         const auto D = U(0.0); // enteral feed rate TODO: get from network
         return -d1 * x[fn_P1] + D;
     }
 
-    auto P2_dot(const auto &x, auto t)
+    auto P2_dot(const std::vector<U> &x)
     {
-        return -P_min(x, t) + d1 * x[fn_P1];
+        return -P_min(x) + d1 * x[fn_P1];
     }
 
 public:
     using state_type = std::vector<U>;
     void operator() (const state_type &x, state_type &dxdt, const U t)
     {
-        dxdt[fn_G] = G_dot(x, t);
-        dxdt[fn_Q] = Q_dot(x, t);
-        dxdt[fn_I] = I_dot(x, t);
-        dxdt[fn_P1] = P1_dot(x, t);
-        dxdt[fn_P2] = P2_dot(x, t);
+        dxdt[fn_G] = G_dot(x);
+        dxdt[fn_Q] = Q_dot(x);
+        dxdt[fn_I] = I_dot(x);
+        dxdt[fn_P1] = P1_dot(x);
+        dxdt[fn_P2] = P2_dot(x);
 
         if(std::fmod(t, 10.0) <= 0.1) {
             std::cout << 
@@ -152,6 +153,11 @@ public:
         }
     }
 };
+
+void load_data(std::string filename)
+{
+        
+}
 
 int main(void)
 {
